d95f27028eeff999ec8df08275fc00f3d6dd3db6
Vinícius <fiorivini@hotmail.com>
2026-01-31 13:04:08 -0300

    cmd main.go ok

---
+++ main.go
@@ -0,0 +1,20 @@
+package config
+
+import "os"
+
+type Config struct {
+	AppPort string
+}
+
+func load() Config {
+	return Config{
+		AppPort: getEnv("APP_PORT", "8080"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
